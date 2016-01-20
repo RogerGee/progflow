391eda7c38aa1994a25806dad407d0e85cd03f37
Roger Gee <rpg11a@acu.edu>
2016-01-20 15:38:13 -0600

    Development changes
        - first commit
        - add basic UI shell
        - add basic terminal functionality

---
+++ src/js/app.js
@@ -0,0 +1,96 @@
+// app.js - progflow
+
+// globals
+var terminal;
+
+// constants
+const VERSION = "0.0";
+const PROGNAME = "ProgFlow Simulator";
+const AGENTINFO = navigator.userAgent;
+
+// initPage() - create page content programmatically
+function initPage() {
+    var controlPanel = document.getElementById('div-control-panel');
+    var terminalView = document.getElementById('div-terminal-view');
+    controlPanel.addBreak = function() {
+        //this.appendChild(document.createElement("br"));
+        this.appendChild(document.createElement("hr"));
+    }
+    controlPanel.addTextField = function(id) {
+        var ed = document.createElement("input");
+        this.appendChild(ed);
+        ed.type = 'text';
+        ed.className = 'control-panel-text-entry';
+        ed.id = id;
+
+        var ws = (this.offsetWidth - ed.previousSibling.offsetWidth-12) + "px";
+        ed.style.width = ws;
+        this.appendChild(document.createElement("br"));
+    }
+    controlPanel.addButtonA = function(label,callback) {
+        var btn = document.createElement("button");
+        this.appendChild(btn);
+        btn.type = 'button';
+        btn.className = 'control-panel-button-a';
+        btn.appendChild(document.createTextNode(label));
+        btn.onclick = callback;
+    }
+    controlPanel.addButtonB = function(label,callback) {
+        var btn = document.createElement("button");
+        this.appendChild(btn);
+        btn.type = 'button';
+        btn.className = 'control-panel-button-b';
+        btn.appendChild(document.createTextNode(label));
+        btn.onclick = callback;
+    }
+    controlPanel.addLabel = function(label) {
+        var lbl = document.createTextNode(label);
+        this.appendChild(lbl);
+        lbl.className = 'control-panel-label';
+    }
+
+    // create control panel buttons
+    controlPanel.addButtonB("new");
+    controlPanel.addTextField("flowchart-name");
+    controlPanel.addButtonB("save");
+    controlPanel.addButtonB("open in new tab");
+    controlPanel.addButtonB("close project");
+    controlPanel.addBreak();
+    controlPanel.addButtonA("in");
+    controlPanel.addButtonA("out");
+    controlPanel.addButtonA("if");
+    controlPanel.addButtonA("while");
+    controlPanel.addButtonA("for");
+    controlPanel.addButtonA("call");
+    controlPanel.addButtonA("proc");
+    controlPanel.addButtonA("+");
+    controlPanel.addButtonA("-");
+    controlPanel.addButtonA("*");
+    controlPanel.addButtonA("÷");
+    controlPanel.addButtonA("mod");
+    controlPanel.addBreak();
+    controlPanel.addButtonB("reset");
+    controlPanel.addButtonB("C++");
+    controlPanel.addButtonB("exec");
+    controlPanel.addButtonB("trace");
+
+    // create terminal content
+    terminal = new Terminal(terminalView);
+    terminal.addLine(PROGNAME + " " + VERSION);
+    terminal.addLine(AGENTINFO);
+}
+
+// resizePage() - handle window resize event
+function resizePage() {
+    // resize all control panel text areas
+    var controlPanel = document.getElementById('div-control-panel');
+    var nodes = controlPanel.getElementsByClassName('control-panel-text-entry');
+    for (var i = 0;i < nodes.length;i++) {
+        var w = controlPanel.offsetWidth-nodes[i].previousSibling.offsetWidth-12;
+        nodes[i].style.width = w + "px";
+    }
+}
+
+// setup event handlers
+window.onload = initPage;
+window.onresize = resizePage;
---
+++ src/js/terminal.js
@@ -0,0 +1,246 @@
+// terminal.js - progflow
+
+function Terminal(parentNode) {
+    function TerminalLine() {
+        var text = '';
+        var curnode = document.createTextNode('');
+        var span = document.createElement("span");
+        span.appendChild(curnode);
+
+        this.addText = function(textParam) {
+            text += textParam;
+            curnode.data += textParam;
+        };
+
+        this.addFormattedText = function(textParam,format) {
+            // create span for formatted text
+            var subspan = document.createElement("span");
+            if (format['color']) {
+                subspan.style.color = format['color'];
+            }
+            if (format['bold']) {
+                subspan.style.fontWeight = 'bold';
+            }
+            if (format['ul']) {
+                subspan.style.textDecoration = 'underline';
+            }
+
+            // add the sub-span to the line
+            text += textParam;
+            subspan.appendChild(document.createTextNode(textParam));
+            span.appendChild(subspan);
+
+            // create new current text node context for future insertions
+            curnode = document.createTextNode('');
+            span.appendChild(subspan);
+        }
+
+        this.finish = function() {
+            termDiv.appendChild(span);
+            termDiv.appendChild(document.createElement("br"));
+        }
+    }
+
+    // create terminal division
+    var termDiv = document.createElement("div");
+    termDiv.className = 'terminal';
+
+    // create cursor element
+    var cursor = document.createElement("span");
+    var cursorText = document.createTextNode(' '); // single character
+    cursor.className = 'terminal-cursor';
+    cursor.appendChild(cursorText);
+
+    // define input mode state
+    var imode = false;
+    var imodeCb = null;
+    var ispan = document.createElement("span");
+    var ispanBefore = document.createTextNode(''); // before cursor
+    var ispanAfter = document.createTextNode(''); // after cursor
+    var itext = "";
+    var ipos = 0;
+    ispan.className = "terminal-input";
+    ispan.appendChild(ispanBefore);
+    ispan.appendChild(cursor);
+    ispan.appendChild(ispanAfter);
+
+    // modify our parent
+    parentNode.appendChild(termDiv);
+    parentNode.onfocus = onFocus;
+    parentNode.onblur = onBlur;
+    parentNode.onkeypress = onKeyPress;
+
+    // onFocus() - handle changes when the terminal is focused
+    function onFocus(e) {
+        cursor.className = 'terminal-cursor';
+    }
+
+    // onBlur() - handle changes when the terminal loses focus
+    function onBlur(e) {
+        cursor.className = 'terminal-cursor terminal-cursor-nofocus';
+    }
+
+    // onKeyPress() - handle input key presses for text entry and control
+    function onKeyPress(e) {
+        // do not handle key presses if not in input mode
+        if (!imode) {
+            return;
+        }
+
+        if (e.charCode != 0) { // printable character
+            // insert character it into the input line text variable and also
+            // update the span's elements for display
+            var before = itext.substr(0,ipos) + e.key; // insert before cursor
+            var after = itext.substr(ipos,itext.length - ipos);
+            itext = before + after;
+            ispanBefore.data = before;
+            ispanAfter.data = after.substr(1); // skip cursor character
+            ipos += 1;
+        }
+        else if (e.keyCode == KEYCODES.ENTER) {
+            e.preventDefault();
+            imodeCb(cancelInput());
+        }
+        else if (e.keyCode == KEYCODES.LEFT) {
+            // move the cursor left one position (if possible)
+            if (ipos > 0) {
+                ipos -= 1;
+
+                // update cursor: the cursor will always assume a character from
+                // the input text
+                var before = itext.substr(0,ipos);
+                var after = itext.substr(ipos,itext.length - ipos);
+                ispanBefore.data = before;
+                ispanAfter.data = after.substr(1);
+                cursorText.data = after[0];
+            }
+        }
+        else if (e.keyCode == KEYCODES.RIGHT) {
+            // move the cursor right one position (if possible); the cursor may
+            // sit at the first invalid position after the input text (where text
+            // may be appended to the line)
+            if (ipos < itext.length) {
+                ipos += 1;
+
+                // update cursor: the cursor may be in an invalid position, in
+                // which case the space character MUST be set to its text node
+                var before = itext.substr(0,ipos);
+                var after = itext.substr(ipos,itext.length - ipos);
+                ispanBefore.data = before;
+                ispanAfter.data = after.substr(1);
+                cursorText.data = ipos >= itext.length ? ' ' : after[0];
+            }
+        }
+        else if (e.keyCode == KEYCODES.UP || e.keyCode == KEYCODES.DOWN) {
+            // no action as of now
+            e.preventDefault();
+        }
+        else if (e.keyCode == KEYCODES.BACKSPACE) {
+            // move the cursor back a position and remove the character at that
+            // position
+            if (ipos > 0) {
+                ipos -= 1;
+
+                var before = itext.substr(0,ipos);
+                var after = itext.substr(ipos+1,itext.length - ipos);
+                ispanBefore.data = before;
+                ispanAfter.data = after.substr(1);
+                itext = before + after;
+                cursorText.data = after[0];
+            }
+        }
+        else if (e.keyCode == KEYCODES.HOME) {
+            // place cursor at the beginning of a line
+            ipos = 0;
+            ispanBefore.data = "";
+            ispanAfter.data = itext.substr(1);
+            cursorText.data = itext[0];
+        }
+        else if (e.keyCode == KEYCODES.END) {
+            // place cursor at the end of a line
+            ipos = itext.length;
+            ispanBefore.data = itext;
+            ispanAfter.data = "";
+            cursorText.data = " ";
+        }
+    }
+
+    ////////////////////////////////////////////////////////////////////////////
+    // Public interface functions
+    ////////////////////////////////////////////////////////////////////////////
+
+    // addLine() - add text to the terminal at the current position and seek
+    // to a new line
+    function addLine(text) {
+        var line = new TerminalLine;
+        line.addText(text);
+        line.finish();
+    }
+    this.addLine = addLine;
+
+    // newLine() - creates a new TerminalLine object and returns it to the user
+    function newLine() {
+        var line = new TerminalLine;
+        return line;
+    }
+    this.newLine = newLine;
+
+    // inputMode() - sets the terminal to input mode; the callback is called once
+    // the user has finished input; the terminal behaves canonically and returns
+    // input after the user has finished entering a line
+    function inputMode(callback) {
+        imode = true;
+        imodeCb = callback;
+
+        // rest the state of the input line
+        itext = "";
+        ipos = 0;
+        ispanBefore.data = "";
+        ispanAfter.data = "";
+        cursorText = " ";
+
+        // add the input line span to the terminal screen
+        termDiv.appendChild(ispan);
+        parentNode.focus(); // set focus so user knows input is requested
+    }
+    this.inputMode = inputMode;
+
+    // cancelInput() - cancels input mode (if on) and returns whatever input
+    // the user has specified
+    function cancelInput() {
+        if (imode) {
+            // exit input mode by toggling the flag and removing the input line
+            // element from the terminal screen
+            imode = false;
+            termDiv.removeChild(ispan);
+            return itext;
+        }
+    }
+    this.cancelInput = cancelInput;
+}
+
+// enumerate useful keycodes
+const KEYCODES = {
+    BACKSPACE: 8,
+    COMMA: 188,
+    DELETE: 46,
+    DOWN: 40,
+    END: 35,
+    ENTER: 13,
+    ESCAPE: 27,
+    HOME: 36,
+    LEFT: 37,
+    NUMPAD_ADD: 107,
+    NUMPAD_DECIMAL: 110,
+    NUMPAD_DIVIDE: 111,
+    NUMPAD_ENTER: 108,
+    NUMPAD_MULTIPLY: 106,
+    NUMPAD_SUBTRACT: 109,
+    PAGE_DOWN: 34,
+    PAGE_UP: 33,
+    PERIOD: 190,
+    RIGHT: 39,
+    SPACE: 32,
+    TAB: 9,
+    UP: 38
+};
